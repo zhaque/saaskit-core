aa223e8a8633e0a01fb59ff1a2b5b50b26ce9665
Maciej Pasternacki <maciej@pasternacki.net>
2009-06-23 16:44:22 +0200

    - Split one project into two projects: for main site and for subdomains, which lets us avoid setting request.urlconf (that breaks stuff);
    - Bump django-muaccounts, use MUACCOUNTS_DEFAULT_URL setting

---
+++ projects/subdomains/settings.py
@@ -0,0 +1,32 @@
+from common_settings import *
+
+import os.path
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+SITE_ID = 1
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '+*q3$z(d1@hi^p%645&636$n7r@=w!m)(z9@k9&9s9_7uh%a+s'
+
+MIDDLEWARE_CLASSES += ('muaccounts.middleware.MUAccountsMiddleware',)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
+) + TEMPLATE_DIRS
--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
-# Django settings for project project.
+# This module is available as common_settings from projects' settings module.
+# It contains settings used in all projects.
 
 import os.path
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+from django.conf import global_settings
+
+KIT_ROOT=os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -13,7 +16,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'django-saas-kit.db'  # Or path to database file if using sqlite3.
+DATABASE_NAME = os.path.join(KIT_ROOT, 'crowdsense.db')  # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -30,28 +33,10 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, '..', 'static')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '+*q3$z(d1@hi^p%645&636$n7r@=w!m)(z9@k9&9s9_7uh%a+s'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -60,33 +45,23 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.load_template_source',
 )
 
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django_authopenid.middleware.OpenIDMiddleware',
-    'muaccounts.middleware.MUAccountsMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'django.middleware.locale.LocaleMiddleware',
 )
 
-ROOT_URLCONF = 'project.urls'
-
-import os.path
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.normpath((os.path.join(os.path.dirname(__file__),'..','templates'))),
-)
+ROOT_URLCONF = 'urls'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
     'paypal.standard.ipn',
     'registration',
     'django_authopenid',
@@ -105,17 +80,19 @@ INSTALLED_APPS = (
     'crowdsense',
 )
 
-from django.conf import global_settings
 TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
     'django.core.context_processors.request',
     'django_authopenid.context_processors.authopenid',
     'pages.context_processors.media',
     )
 
+TEMPLATE_DIRS = ( os.path.join(KIT_ROOT, 'templates'), )
+
 AUTH_PROFILE_MODULE = 'crowdsense.UserProfile'
 
 PAGE_TAGGING = True
 PAGE_TINYMCE = False
+PAGE_USE_SITE_ID = True
 PAGE_LANGUAGES = (                      # need to be filled,
     ('en-us', 'US English'),
     ('en-gb', 'British English'),
@@ -131,7 +108,7 @@ SUBSCRIPTION_PAYPAL_SETTINGS = {
     }
 
 MUACCOUNTS_ROOT_DOMAIN = 'example.com'
-MUACCOUNTS_DEFAULT_DOMAIN = 'www.example.com'
+# MUACCOUNTS_DEFAULT_DOMAIN = 'www.example.com'
+MUACCOUNTS_DEFAULT_URL = 'http://www.example.com:8001/'
 MUACCOUNTS_PORT=8000
-MUACCOUNTS_ACCOUNT_URLCONF = 'project.urls_muaccount'
 MUACCOUNTS_IP = '127.0.0.1'

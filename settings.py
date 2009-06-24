d5ac5446dddb23609a5c47800b09fa9b164de7c4
Maciej Pasternacki <maciej@pasternacki.net>
2009-06-24 14:58:31 +0200

    Add django-ab

--- settings.py
+++ settings.py
@@ -40,6 +40,7 @@ USE_I18N = True
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
+    'ab.loaders.load_template_source',
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
 #     'django.template.loaders.eggs.load_template_source',
@@ -54,6 +55,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.doc.XViewMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'ab.middleware.ABMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
@@ -80,6 +82,7 @@ INSTALLED_APPS = (
     'filter',
     'ajax_validation',
     'uni_form',
+    'ab',
     'quotas',
     'muaccounts',
     'subscription',

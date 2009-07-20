60754e96891735f166a9eb0c6f6ab5525f0ab966
Maciej Pasternacki <maciej@pasternacki.net>
2009-07-20 02:55:31 +0200

    Basic public/private user profiles.

--- crowdsense/user_profile.py
+++ crowdsense/user_profile.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+# HACK: reuse third-party code used in muaccounts
+from muaccounts.model_fields import RemovableImageField
+
+def _photo_path(instance, filename):
+    return 'profile-photos/%d.jpg' % instance.pk
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     real_name = models.CharField(max_length=100)
-    nickname = models.CharField(max_length=100)
-    # ...
+    about = models.TextField(blank=True)
+    is_public = models.BooleanField()
 
     @models.permalink
     def get_absolute_url(self):

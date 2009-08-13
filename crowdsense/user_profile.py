0a9856daa39c8c7c89e6259d9c115bee3ad6c5e1
CrowdSense <admin@crowdsense.com>
2009-08-13 22:28:41 +0100

    user profile fix

--- crowdsense/user_profile.py
+++ crowdsense/user_profile.py
@@ -1,12 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# HACK: reuse third-party code used in muaccounts
-from muaccounts.model_fields import RemovableImageField
-
-def _photo_path(instance, filename):
-    return 'profile-photos/%d.jpg' % instance.pk
-
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     real_name = models.CharField(max_length=100)

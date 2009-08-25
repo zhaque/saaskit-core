0346bd7e6ac6acd0feb74fe34d0917ee04ee8028
CrowdSense <admin@crowdsense.com>
2009-08-25 22:43:59 +0100

    removing yotweets sample templates

--- projects/user_sites/urls.py
+++ projects/user_sites/urls.py
@@ -6,7 +6,6 @@ handler404 = 'perfect404.views.page_not_found'
 urlpatterns = patterns('',
     url(r'^$', 'django.views.generic.simple.direct_to_template', dict(template='index.html'), name='notification_notices'), # name to work around django-notification's brain damage
     url(r'^sso/$', 'sso.views.sso', name="sso"),
-    (r'^results/$', 'django.views.generic.simple.direct_to_template', dict(template='results.html')),
     (r'^sorry/$', 'django.views.generic.simple.direct_to_template', dict(template='account_nam.html'), 'muaccounts_not_a_member'),
     (r'^accounts/', include('django_authopenid.urls')),
     (r'^admin/', include('muaccounts.urls')),

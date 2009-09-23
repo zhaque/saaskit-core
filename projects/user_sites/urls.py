7f56f8664039a4a87488ebd4143dab6d4f28fd08
CrowdSense Admin <admin@crowdsense.com>
2009-09-23 16:39:54 +0000

    work in progress in terms new feature

--- projects/user_sites/urls.py
+++ projects/user_sites/urls.py
@@ -4,11 +4,16 @@ from django.conf.urls.defaults import *
 handler404 = 'perfect404.views.page_not_found'
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.views.generic.simple.direct_to_template', dict(template='index.html'), name='notification_notices'), # name to work around django-notification's brain damage
+    url(r'^$', 'django.views.generic.simple.direct_to_template', dict(template='index.html'), name='notification_notices'),
     url(r'^sso/$', 'sso.views.sso', name="sso"),
     (r'^sorry/$', 'django.views.generic.simple.direct_to_template', dict(template='account_nam.html'), 'muaccounts_not_a_member'),
     (r'^accounts/', include('django_authopenid.urls')),
     (r'^admin/', include('muaccounts.urls')),
+    url(r'^extend/general/$', 'django.views.generic.simple.direct_to_template', dict(template='manage_general.html')),
+    url(r'^extend/themes/$', 'django.views.generic.simple.direct_to_template', dict(template='manage_themes.html')),
+    url(r'^extend/users/$', 'django.views.generic.simple.direct_to_template', dict(template='manage_users.html')),
+    url(r'^extend/advanced/$', 'django.views.generic.simple.direct_to_template', dict(template='manage_advanced.html')),
+    url(r'^extend/apps/$', 'django.views.generic.simple.direct_to_template', dict(template='manage_apps.html')),
 )
 
 # serve static files in debug mode

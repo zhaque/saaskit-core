055f58a955298ad31b26fbe40079fca3a6ff6d56
Sidorenko Vladimir <yoyavova@gmail.com>
2009-10-05 19:14:06 +0300

    added package info

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="saaskit-core",
+           version="0.1",
+           description="Core of saaskit",
+           author="CrowdSense",
+           author_email="admin@crowdsense.com",
+           packages=find_packages(),
+           include_package_data=True,
+)
+

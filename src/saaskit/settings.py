15cae165b0ca29823a06b2fb12c2b5a33e7b14da
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-10-20 17:20:25 +0300

    changed media linking

--- src/saaskit/settings.py
+++ src/saaskit/settings.py
@@ -37,6 +37,11 @@ LANGUAGE_CODE = 'en-gb'
 # to load the internationalization machinery.
 USE_I18N = True
 
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(KIT_ROOT, 'wide_media')
+APP_MEDIA_ROOT = MEDIA_ROOT
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"

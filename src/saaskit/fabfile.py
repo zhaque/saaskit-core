43be50f070c79050aa879342871344d642de18b3
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-10-21 18:35:58 +0300

    fabric corrections

--- src/saaskit/fabfile.py
+++ src/saaskit/fabfile.py
@@ -12,11 +12,34 @@ def render_put(template_name, dest, params, mode=None, tempfile='tempfile'):
     put(tempfile, dest, mode)
     os.remove(tempfile)
 
+def production():
+    #env.hosts = []
+    
+    if not env.hosts:
+        prompt("No hosts found. Please specify (single) host string for connection", 'host_string', 'yotweets.com')
+    
+    if not env.user:
+        prompt("SSH User", 'user', 'root')
+    
+    if not ('VPS_IP' in env and env.VPS_IP):
+        prompt("VPS IP", 'VPS_IP', '97.107.138.174')
+    
+    if not ('POSTGRES_USER' in env and env.POSTGRES_USER):
+        prompt("Postgresql username", 'POSTGRES_USER', 'saaskit')
+    
+    if not ('POSTGRES_PASSWORD' in env and env.POSTGRES_PASSWORD):
+        prompt("Postgresql user's password", 'POSTGRES_PASSWORD', 'saaskitS3n89mkk')
+    
+    if not ('POSTGRES_DB' in env and env.POSTGRES_DB):
+        prompt("Postgresql DATABASE", 'POSTGRES_DB', 'saaskit')
+        
+    env.UBUNTU_VERSION = 'jaunty'
+
 def install_packages():
     """Install system wide packages"""
     require('hosts',provided_by=["production"])
     
-    render_put('deploy/apt/sources.list', '/etc/apt/sources.list.d/sources.list')
+    render_put('deploy/apt/sources.list', '/etc/apt/sources.list.d/sources.list', env)
     sudo('apt-get -y update; apt-get -y upgrade;', pty=True)
     #locale
     sudo('dpkg-reconfigure locales; apt-get install -y language-pack-en; locale-gen en_US.UTF-8;', pty=True)
@@ -47,9 +70,9 @@ def github_config():
     
     #ssh public keys
     run('mkdir -p ~/.ssh', pty=True)
-    render_put('deploy/.ssh/id_rsa', '~/.ssh/id_rsa')
-    render_put('deploy/.ssh/id_rsa.pub', '~/.ssh/id_rsa.pub')
-    render_put('deploy/.ssh/known_hosts', '~/.ssh/known_hosts')
+    render_put('deploy/.ssh/id_rsa', '~/.ssh/id_rsa', env)
+    render_put('deploy/.ssh/id_rsa.pub', '~/.ssh/id_rsa.pub', env)
+    render_put('deploy/.ssh/known_hosts', '~/.ssh/known_hosts', env)
 
 def postgresql_setup():
     require('hosts',provided_by=["production"])
@@ -57,7 +80,7 @@ def postgresql_setup():
     
     sudo('apt-get -y install postgresql-8.3 postgresql-client-8.3 libpq-dev', pty=True)
     
-    render_put('deploy/postgresql/pg_hba.conf', '/etc/postgresql/8.3/main/pg_hba.conf')
+    render_put('deploy/postgresql/pg_hba.conf', '/etc/postgresql/8.3/main/pg_hba.conf', env)
     sudo('/etc/init.d/postgresql-8.3 restart', pty=True)
     
     run('sudo -u postgres psql -c "create user %s with password \'%s\'"' \
@@ -94,7 +117,7 @@ def install_project():
          % {'name': env.host_string}, pty=True)
     
     #create local settings
-    render_put('deploy/_local_settings.py', '/webapp/%s/src/saaskit/local_settings.py' % env.host_string)
+    render_put('deploy/_local_settings.py', '/webapp/%s/src/saaskit/local_settings.py' % env.host_string, env)
 
     #buildout the project
     sudo('cd /webapp/%s; python ./bootstrap.py -c ./production.cfg; ./bin/buildout -v -c ./production.cfg' % env.host_string, pty=True)
@@ -112,12 +135,12 @@ def nginx_config():
     sudo('apt-get -y install nginx', pty=True)
     sudo('/etc/init.d/nginx stop; rm -f /etc/nginx/sites-enabled/default;', pty=True)
 
-    render_put('deploy/nginx/assets', '/etc/nginx/sites-available/assets')
+    render_put('deploy/nginx/assets', '/etc/nginx/sites-available/assets', env)
     sudo('ln -f -s /etc/nginx/sites-available/assets /etc/nginx/sites-enabled/assets', pty=True)
-    render_put('deploy/nginx/webapp', '/etc/nginx/sites-available/webapp')
+    render_put('deploy/nginx/webapp', '/etc/nginx/sites-available/webapp', env)
     sudo('ln -f -s /etc/nginx/sites-available/webapp /etc/nginx/sites-enabled/webapp', pty=True)
-    render_put('deploy/nginx/nginx.conf', '/etc/nginx/nginx.conf')
-    render_put('deploy/nginx/proxy.conf', '/etc/nginx/proxy.conf')
+    render_put('deploy/nginx/nginx.conf', '/etc/nginx/nginx.conf',env)
+    render_put('deploy/nginx/proxy.conf', '/etc/nginx/proxy.conf', env)
     
     sudo('/etc/init.d/nginx start', pty=True)
 
@@ -126,12 +149,12 @@ def apache2_config():
     sudo('apt-get -y install apache2 apache2.2-common apache2-mpm-worker apache2-threaded-dev libapache2-mod-wsgi libapache2-mod-rpaf', pty=True)
     sudo('apache2ctl stop; a2dissite 000-default;', pty=True)
     
-    render_put('deploy/apache/main', '/etc/apache2/sites-available/main')
-    render_put('deploy/apache/sites', '/etc/apache2/sites-available/sites')
+    render_put('deploy/apache/main', '/etc/apache2/sites-available/main', env)
+    render_put('deploy/apache/sites', '/etc/apache2/sites-available/sites', env)
     sudo('a2ensite main; a2ensite sites;', pty=True)
-    render_put('deploy/apache/apache2.conf', '/etc/apache2/apache2.conf')
-    render_put('deploy/apache/ports.conf', '/etc/apache2/ports.conf')
-    render_put('deploy/apache/security', '/etc/apache2/conf.d/security')
+    render_put('deploy/apache/apache2.conf', '/etc/apache2/apache2.conf', env)
+    render_put('deploy/apache/ports.conf', '/etc/apache2/ports.conf', env)
+    render_put('deploy/apache/security', '/etc/apache2/conf.d/security', env)
     
     sudo('a2enmod rewrite; apache2ctl start;', pty=True)
 

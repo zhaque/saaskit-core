d6011491ab523a86121ed3e997f8086ae5cfb583
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-10-21 18:15:27 +0300

    added head script for fabric deployment

--- src/saaskit/fabfile.py
+++ src/saaskit/fabfile.py
@@ -1,7 +1,5 @@
 ### -*- coding: utf-8 -*- ##
 
-#python 2.5 backward compatibility
-from __future__ import with_statement
 import os
 
 from django.template import loader
@@ -14,29 +12,6 @@ def render_put(template_name, dest, params, mode=None, tempfile='tempfile'):
     put(tempfile, dest, mode)
     os.remove(tempfile)
 
-def production():
-    #env.hosts = []
-    
-    if not env.hosts:
-        prompt("No hosts found. Please specify (single) host string for connection", 'host_string', 'yotweets.com')
-    
-    if not env.user:
-        prompt("SSH User", 'user', 'root')
-    
-    if not ('VPS_IP' in env and env.VPS_IP):
-        prompt("VPS IP", 'VPS_IP', '97.107.138.174')
-    
-    if not ('POSTGRES_USER' in env and env.POSTGRES_USER):
-        prompt("Postgresql username", 'POSTGRES_USER', 'saaskit')
-    
-    if not ('POSTGRES_PASSWORD' in env and env.POSTGRES_PASSWORD):
-        prompt("Postgresql user's password", 'POSTGRES_PASSWORD', 'saaskitS3n89mkk')
-    
-    if not ('POSTGRES_DB' in env and env.POSTGRES_DB):
-        prompt("Postgresql DATABASE", 'POSTGRES_DB', 'saaskit')
-        
-    env.UBUNTU_VERSION = 'jaunty'
-    
 def install_packages():
     """Install system wide packages"""
     require('hosts',provided_by=["production"])

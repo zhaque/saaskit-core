ecfd17313dbf2252585997ecaf43a29d441962bb
CrowdSense <admin@DevBook.home>
2009-12-26 11:04:54 +0000

    fixing class_if_reverse for more then one level

--- src/saaskit/templatetags/class_if_reverse.py
+++ src/saaskit/templatetags/class_if_reverse.py
@@ -8,7 +8,7 @@ class ClassIfReverseNode(template.Node):
         self.class_name = class_name
         self.url_path = url_path
     def render(self, context):
-        if 'request' in context and context['request'].path.startswith(self.url_path):
+        if 'request' in context and context['request'].path.endswith(self.url_path):
             return ' class="%s"' % self.class_name
         else: return ''
 
@@ -16,7 +16,7 @@ def do_class_if_reverse(parser, token):
     try: tag_name, url, cls = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError, "%r tag requires two arguments: url, css class" % token.contents.split()[0]
-    if not url.startswith('/'): url = reverse(url)
+    if not url.endswith('/'): url = reverse(url)
     return ClassIfReverseNode(url, cls)
 
 register.tag('class_if_reverse', do_class_if_reverse)

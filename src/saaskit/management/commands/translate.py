6edb5e26b09ec05e77bea7b3b94ef0658801e74f
Stepan Rakhimov <strakh@macbook2.local>
2010-04-26 20:45:45 +0400

    added params to translate

--- src/saaskit/management/commands/translate.py
+++ src/saaskit/management/commands/translate.py
@@ -3,13 +3,27 @@ from django.conf import settings
 from django.core import management
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import CommandError
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import LabelCommand
 from saaskit.trans import run
 
-class Command(NoArgsCommand):
+class Command(LabelCommand):
   help = 'Translate all .po files using Google Translate.'
-  def handle_noargs(self, **options):
-      self.translate()
+  args = 'available lang from setting.py or "all"'
+  label = 'available lang from setting.py or "all"'
 
-  def translate(self):
-    run()
+  ALL = 'all'
+
+  def handle_label(self, label, **options):
+    lang_exists = False
+    for lang in settings.LANGUAGES:
+      if label in lang:
+        lang_exists = True
+    if lang_exists or label==self.ALL:
+      self.translate(label)
+    else:
+      print 'Language "%s" was not found in settings.py' % label
+
+  def translate(self, lang):
+    if lang == self.ALL:
+      lang=None
+    run(lang)
